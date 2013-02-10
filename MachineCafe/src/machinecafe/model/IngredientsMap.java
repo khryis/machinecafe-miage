3b46f4b0be7ac558012384e0b70e853e9dc149a5
chris.dealm@gmail.com <chris.dealm@gmail.com@33542ae2-2f16-765c-9522-2d6b51358b23>
2013-02-10 17:20:36 +0000

    Add comments and some changes

---
+++ MachineCafe/src/machinecafe/model/IngredientsMap.java
@@ -0,0 +1,13 @@
+package machinecafe.model;
+
+import machinecafe.model.Ingredient;
+import java.util.HashMap;
+
+public class IngredientsMap extends HashMap<String,Ingredient>{
+    private static final long serialVersionUID = 1L;
+    
+    public void ajouterIngredients(String nom){
+       Ingredient i = new Ingredient(nom);
+       this.put(nom, i);
+   } 
+}
